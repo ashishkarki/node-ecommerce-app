b4690b3baa7ed07e606768bb0c95e13c00228bca
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2022-03-09 20:59:55 +0545

    feat: implementing categories add/update/delete endpoints - part 2

--- routes/categories.js
+++ routes/categories.js
@@ -21,6 +21,31 @@ categoryRouter.get('/', async (req, res) => {
     }
 })
 
+// GET category by id
+categoryRouter.get('/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const category = await Category.findById(id)
+
+        if (!category) {
+            return responseBuilder(res, StatusCodes.NOT_FOUND, [], true, {
+                message: 'Category not found for id: ' + id,
+            })
+        } else {
+            return responseBuilder(res, StatusCodes.OK, category)
+        }
+    } catch (err) {
+        return responseBuilder(
+            res,
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            [],
+            true,
+            err
+        )
+    }
+})
+
 // category POST
 categoryRouter.post('/', async (req, res) => {
     const { name, icon, color } = req.body
@@ -35,6 +60,39 @@ categoryRouter.post('/', async (req, res) => {
         const savedCategory = await newCategory.save()
 
         return responseBuilder(res, StatusCodes.CREATED, savedCategory)
+    } catch (err) {
+        return responseBuilder(
+            res,
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            [],
+            true,
+            err
+        )
+    }
+})
+
+// category UPDATE
+categoryRouter.put('/:id', async (req, res) => {
+    const { id } = req.params
+
+    const { name, icon, color } = req.body
+
+    try {
+        const category = await Category.findById(id)
+
+        if (!category) {
+            return responseBuilder(res, StatusCodes.NOT_FOUND, [], true, {
+                message: 'Category not found for id: ' + id,
+            })
+        }
+
+        category.name = name
+        category.icon = icon
+        category.color = color
+
+        const updatedCategory = await category.save()
+
+        return responseBuilder(res, StatusCodes.OK, updatedCategory)
     } catch (err) {
         return responseBuilder(
             res,
