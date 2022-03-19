7da5157426baec50dd42aba106747c09b778fad7
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2022-03-19 23:45:04 +0545

    style(styling-ui): :lipstick: Using 'NavLink' in 'headerNav' component to show active link

--- node-ecommerce-web/src/components/HeaderNav/HeaderNav.jsx
+++ node-ecommerce-web/src/components/HeaderNav/HeaderNav.jsx
@@ -1,8 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 import styles from './HeaderNav.module.css'
 
 const HeaderNav = () => {
+    const setActiveLinkProps = (isActive) => ({
+        color: isActive ? 'green' : 'inherit',
+        fontWeight: isActive ? 'bold' : 'inherit',
+    })
+
+    const createNavLink = (to, label) => (
+        <NavLink
+            to={to}
+            className={styles.navItems}
+            style={({ isActive }) => setActiveLinkProps(isActive)}
+        >
+            {label}
+        </NavLink>
+    )
+
     return (
         <header className="py-2 ash-bg grid grid-cols-1 md:grid-cols-2 gap-1">
             <h2 className="font-semibold text-2xl capitalize pl-4 ash-bg ash-txt">
@@ -10,17 +25,11 @@ const HeaderNav = () => {
             </h2>
 
             <nav className="md:ml-28 flex justify-around">
-                <Link to="/" className={styles.navItems}>
-                    Home
-                </Link>
+                {createNavLink('/', 'Home')}
 
-                <Link to="/products" className={styles.navItems}>
-                    Products
-                </Link>
+                {createNavLink('/products', 'Products')}
 
-                <Link to="/categories" className={styles.navItems}>
-                    Categories
-                </Link>
+                {createNavLink('/categories', 'Categories')}
             </nav>
         </header>
     )
