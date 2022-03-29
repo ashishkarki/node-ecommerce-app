cdad7d64c040c91bceaf82631296347b18585053
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2022-03-29 20:31:06 +0545

    feat(order-related): :construction: implemented Delete Order and its OrderItems

--- routes/order.js
+++ routes/order.js
@@ -15,6 +15,8 @@ const orderRouter = express.Router()
 orderRouter.get('/', async (req, res) => {
     responseBuilderWrapper(async () => {
         const allOrders = await OrderModel.find()
+            .populate('user', 'name')
+            .sort({ dateOrdered: -1 })
 
         if (!allOrders) {
             responseBuilder(res, StatusCodes.NOT_FOUND, [], true, {
@@ -37,6 +39,15 @@ orderRouter.get('/:id', async (req, res) => {
 
     responseBuilderWrapper(async () => {
         const order = await OrderModel.findById(req.params.id)
+            .populate('user', 'name')
+            // .populate('orderItems')
+            .populate({
+                path: 'orderItems',
+                populate: {
+                    path: 'product',
+                    populate: 'category',
+                },
+            })
 
         if (!order) {
             return responseBuilder(res, StatusCodes.NOT_FOUND, [], true, {
@@ -83,6 +94,85 @@ orderRouter.post('/', async (req, res) => {
     })
 })
 
+// 4. Update the Status of the order
+orderRouter.patch('/:id/status', async (req, res) => {
+    // before first check if this id is valid
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return responseBuilder(res, StatusCodes.BAD_REQUEST, [], true, {
+            message: `Invalid Order id: ${req.params.id}`,
+        })
+    }
+
+    responseBuilderWrapper(async () => {
+        const order = await OrderModel.findById(req.params.id)
+
+        if (!order) {
+            return responseBuilder(res, StatusCodes.NOT_FOUND, [], true, {
+                message: `Order not found for id: ${req.params.id}`,
+            })
+        }
+
+        const updatedOrder = await OrderModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                status: req.body.status,
+            },
+            {
+                new: true,
+            }
+        )
+
+        if (!updatedOrder) {
+            return responseBuilder(
+                res,
+                StatusCodes.INTERNAL_SERVER_ERROR,
+                [],
+                true,
+                {
+                    message: `Error updating order`,
+                }
+            )
+        } else {
+            responseBuilder(res, StatusCodes.OK, updatedOrder)
+        }
+    })
+})
+
+// 5. Delete Order
+orderRouter.delete('/:id', async (req, res) => {
+    // before first check if this id is valid
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return responseBuilder(res, StatusCodes.BAD_REQUEST, [], true, {
+            message: `Invalid Order id: ${req.params.id}`,
+        })
+    }
+
+    responseBuilderWrapper(async () => {
+        const order = await OrderModel.findById(req.params.id)
+
+        if (!order) {
+            return responseBuilder(res, StatusCodes.NOT_FOUND, [], true, {
+                message: `Order not found for id: ${req.params.id}`,
+            })
+        }
+
+        const deletedOrder = await OrderModel.findByIdAndDelete(req.params.id)
+
+        if (!deletedOrder) {
+            return responseBuilder(res, StatusCodes.NOT_FOUND, [], true, {
+                message: `Order not found for id: ${req.params.id}`,
+            })
+        } else {
+            // first, also delete the orderItems related to this order
+            deletedOrder.orderItems.map(async (oItemId) => {
+                const deletedOrderItem = await deleteOrderItem(oItemId)
+            })
+
+            responseBuilder(res, StatusCodes.OK, deletedOrder)
+        }
+    })
+})
+
 // Helper function to save OrderItem while saving a complete order
 const saveOrderItem = async (orderItem) => {
     const newOrderItem = new OrderItemModel({
@@ -92,10 +182,22 @@ const saveOrderItem = async (orderItem) => {
     const savedOrderItem = await newOrderItem.save()
 
     if (!savedOrderItem) {
-        throw new Error('Error saving order item')
+        throw new Error('Error saving order item with id: ', orderItem.id)
     }
 
     return savedOrderItem
+}
+
+// Helper funciton to delete OrderItem while deleting the parent Order
+const deleteOrderItem = async (orderItemId) => {
+    // console.log(`orderItemId: ${orderItemId}`)
+    const deletedOrderItem = await OrderItemModel.findByIdAndDelete(orderItemId)
+
+    if (!deletedOrderItem) {
+        throw new Error('Error deleting order item with id: ', orderItemId)
+    }
+
+    return deletedOrderItem
 }
 
 // Helper function to build a response using the responseBuilder
